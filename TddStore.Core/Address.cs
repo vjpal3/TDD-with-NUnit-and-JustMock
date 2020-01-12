1a719a3e83afb36a49a97fb625872a076a6788cf
vjpal3 <vrishalipal@yahoo.com>
2020-01-12 17:53:09 -0500

    Add customer service for testing order fulfillment

---
+++ TddStore.Core/Address.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddStore.Core
+{
+    public class Address
+    {
+        public string StreetAddressOne { get; set; }
+        public string StreetAddressTwo { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+    }
+}
---
+++ TddStore.Core/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TddStore.Core
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address ShippingAddress { get; private set; }
+
+        public Customer()
+        {
+            ShippingAddress = new Address();
+        }
+    }
+}
